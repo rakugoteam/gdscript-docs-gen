41b4943ee99ff9ffb698f0a1c353ce0619ef4376
Jeremi Biernacki <jeremi360@gmail.com>
2023-12-23 08:59:25 +0100

    separate -c flag to separated check_gds_docs.py

---
+++ check_gds_docs.py
@@ -0,0 +1,33 @@
+
+from gdscript_doc_tools import *
+
+if __name__ == "__main__":
+	args = {}
+	get_argv(args)
+	# print(args)
+
+	if args["help"]:
+		print(
+			"""
+			check_gds_docs.py version 1.0
+			It is a simple python tool that checks gdscript doc comments.
+			It will show you which const, vars, signals and funcs are missing docs comments.
+
+			Example of usage:
+				python check_gds_docs.py -ir gds_source -s plugin.gd
+
+			Flags:
+				-h, --help		will display this message
+				-i, --input		you give dir sources of gdscript script to scan
+				-r, --recursive		scan input recusivly
+				-s, --skip		files and dirs to skip from input
+			"""
+		)
+		exit()
+
+	for i in args["input"]:
+		scripts = scandir_for_gdscipts(
+			i, args["recursive"], args["skip"]
+		)
+	
+	check_gds_docs(scripts)
---
+++ gds_docs2md.py
@@ -0,0 +1,32 @@
+
+from gdscript_doc_tools import *
+
+if __name__ == "__main__":
+	args = {}
+	get_argv(args)
+	# print(args)
+
+	if args["help"]:
+		print(
+			"""
+			gds_docs2md.py version 1.0
+			It is a simple python tool that generates markdown from gdscript doc comments.
+
+			Example of usage:
+				python gds_docs2md.py -o output -ir gds_source -s plugin.gd
+
+			Flags:
+				-h, --help		will display this message
+				-i, --input		you give dir sources of gdscript script to scan
+				-r, --recursive		scan input recusivly
+				-s, --skip		files and dirs to skip from input
+			"""
+		)
+		exit()
+
+	for i in args["input"]:
+		scripts = scandir_for_gdscipts(
+			i, args["recursive"], args["skip"]
+		)
+	
+	gen_docs(scripts, args["output"])
